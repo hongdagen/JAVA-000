0aad7c1db95aef99fa1089af75051ea24ce3343e
hongdagen <hongyixyz@163.com>
2020-11-10 21:50:36 +0800

    1105homework 更新

--- Week_04/ConcurrentExercise/src/main/java/com/hyhy/concurrent/exercise/countdownlatch/Main.java
+++ Week_04/ConcurrentExercise/src/main/java/com/hyhy/concurrent/exercise/countdownlatch/Main.java
@@ -1,21 +1,45 @@
 package com.hyhy.concurrent.exercise.countdownlatch;
 
-import com.hyhy.concurrent.exercise.BaseMethod;
 
 import java.util.concurrent.CountDownLatch;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    private static Integer value;
+
+
+    public static void main(String[] args) throws Exception {
         final CountDownLatch cdl = new CountDownLatch(1);
+        long start = System.currentTimeMillis();
+        // 在这里创建一个线程或线程池，
+        // 异步执行 下面方法
+
+        Main main = new Main();
+
         new Thread(new Runnable() {
             public void run() {
-                Thread.currentThread().setName("countdownlatch的新线程");
-                System.out.println(BaseMethod.print(Thread.currentThread().getName()));
+                sum();
                 cdl.countDown();
             }
         }).start();
+
         cdl.await();
-        System.out.println("主线程结束");
+        // 确保  拿到result 并输出
+        System.out.println("异步计算结果为：" + value);
+
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+
+        // 然后退出main线程
+    }
+
+    private static int sum() {
+        value = fibo(6);
+        return value;
+    }
+
+    private static int fibo(int a) {
+        if (a < 2)
+            return 1;
+        return fibo(a - 1) + fibo(a - 2);
     }
 }

3724fba37339f01b5bd2af7599854227ba4cbdb1
hongdagen <hongyixyz@163.com>
2020-11-11 19:38:29 +0800

    1105homework 更新join方法

--- Week_04/ConcurrentExercise/src/main/java/com/hyhy/concurrent/exercise/join/Join.java
+++ Week_04/ConcurrentExercise/src/main/java/com/hyhy/concurrent/exercise/join/Join.java
@@ -6,29 +6,38 @@ package com.hyhy.concurrent.exercise.join;
  */
 public class Join {
 
-    public static void main(String[] args) {
+    private static Integer value;
 
-        long start=System.currentTimeMillis();
+    public static void main(String[] args) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
         // 在这里创建一个线程或线程池，
         // 异步执行 下面方法
 
-        int result = sum(); //这是得到的返回值
+
+        Thread thread = new Thread(new Runnable() {
+            public void run() {
+                value = sum();
+            }
+        });
+        thread.start();
+        thread.join();
 
         // 确保  拿到result 并输出
-        System.out.println("异步计算结果为："+result);
+        System.out.println("异步计算结果为：" + value);
 
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
 
         // 然后退出main线程
     }
 
     private static int sum() {
-        return fibo(36);
+        return fibo(5);
     }
 
     private static int fibo(int a) {
-        if ( a < 2)
+        if (a < 2)
             return 1;
-        return fibo(a-1) + fibo(a-2);
+        return fibo(a - 1) + fibo(a - 2);
     }
 }

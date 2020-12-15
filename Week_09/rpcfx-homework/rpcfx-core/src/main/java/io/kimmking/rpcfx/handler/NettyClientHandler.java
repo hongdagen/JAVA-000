0d88351c7981df8a53ea08661337471374ff6b4a
hongdagen <hongyixyz@163.com>
2020-12-15 22:22:49 +0800

    1212 homework1

---
+++ Week_09/rpcfx-homework/rpcfx-core/src/main/java/io/kimmking/rpcfx/handler/NettyClientHandler.java
@@ -0,0 +1,58 @@
+package io.kimmking.rpcfx.handler;
+
+import com.alibaba.fastjson.JSON;
+import io.kimmking.rpcfx.api.RpcfxRequest;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @Author: hyhy
+ * @Date: 2020/12/15 7:59 下午
+ */
+
+public class NettyClientHandler extends ChannelInboundHandlerAdapter {
+
+    private RpcfxRequest req;
+
+    public NettyClientHandler(RpcfxRequest req){
+        this.req = req;
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        URI uri = new URI("/");
+        String data = JSON.toJSONString(req);
+        ByteBuf bbuf = Unpooled.copiedBuffer(data, StandardCharsets.UTF_8);
+        FullHttpRequest request = new DefaultFullHttpRequest(
+                HttpVersion.HTTP_1_1, HttpMethod.POST, uri.toASCIIString(),bbuf);
+
+
+        request.headers().set("Host", "127.0.0.1:8080");
+        request.headers().set("Connection", HttpHeaderValues.KEEP_ALIVE);
+        request.headers().set("Content-Type", "application/json");
+
+        request.headers().set("Content-Length", bbuf.readableBytes());
+
+        ctx.writeAndFlush(request);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println("msg -> "+msg);
+        if(msg instanceof FullHttpResponse){
+            FullHttpResponse response = (FullHttpResponse)msg;
+            ByteBuf buf = response.content();
+            String result = buf.toString(CharsetUtil.UTF_8);
+            System.out.println("response -> "+result);
+        }
+
+    }
+}
+
--- Week_09/rpcfx-homework/rpcfx-core/src/main/java/io/kimmking/rpcfx/handler/NettyReqClient.java
+++ Week_09/rpcfx-homework/rpcfx-core/src/main/java/io/kimmking/rpcfx/handler/NettyReqClient.java
@@ -1,8 +1,57 @@
 package io.kimmking.rpcfx.handler;
 
+import com.alibaba.fastjson.JSON;
+import io.kimmking.rpcfx.api.RpcfxRequest;
+import io.kimmking.rpcfx.api.RpcfxResponse;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.HeadersUtils;
+import io.netty.handler.codec.http.*;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * @Author: hyhy
  * @Date: 2020/12/15 8:53
  */
 public class NettyReqClient {
+
+    public RpcfxResponse post(RpcfxRequest req, String url) {
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(new NioEventLoopGroup());
+            bootstrap.channel(NioSocketChannel.class);
+
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    ChannelPipeline pipeline = ch.pipeline();
+                    pipeline.addLast(new LoggingHandler(LogLevel.INFO));
+                    pipeline.addLast(new NettyClientHandler(req));
+                }
+            });
+            ChannelFuture f = bootstrap.connect("127.0.0.1", 8080).sync();
+            f.channel().closeFuture().sync();
+
+        } catch ( InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+
+        }
+        return null;
+    }
 }
